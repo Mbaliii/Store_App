36a005f7a182fde3d7f823039359359c607ee57c
Mbali <mbalikhumalo408@gmail.com>
2023-08-10 10:12:22 +0200

    store

---
+++ components/products/ProductRow.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+const ProductRow = () => {
+    return (
+        <View>
+            <Text>Product View Page</Text>
+        </View>
+    )
+}
+
+export default ProductRow
--- screens/Home.js
+++ screens/Home.js
@@ -5,6 +5,7 @@ import styles from "./homeStyle";
 import { Ionicons, Fontisto } from "@expo/vector-icons";
 import { Welcome } from "../components";
 import Carousel from "../components/home/Carousel";
+import Headings from "../components/home/Headings";
 
 
 
@@ -30,6 +31,7 @@ const Home = () => {
             <ScrollView>
                 <Welcome />
                 <Carousel/>
+                <Headings/>
             </ScrollView>
         </SafeAreaView>
     )
